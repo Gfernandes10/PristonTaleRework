b6495db81e79d9fc8b273ed874f7c543519ab227
Gfernandes10 <gfernandes10@hotmail.com.br>
2025-10-14 14:45:55 -0400

    test

--- Source/PristonTaleRework/PlayerCharacter.h
+++ Source/PristonTaleRework/PlayerCharacter.h
@@ -13,5 +13,32 @@ UCLASS()
 class PRISTONTALEREWORK_API APlayerCharacter : public ABaseCharacter
 {
 	GENERATED_BODY()
+
+private:
+
+	/** Top down camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components", meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* TopDownCameraComponent;
+
+	/** Camera boom positioning the camera above the character */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components", meta = (AllowPrivateAccess = "true"))
+	class USpringArmComponent* CameraBoom;
+
+public:
+
+	/** Constructor */
+	APlayerCharacter();
+
+	/** Initialization */
+	virtual void BeginPlay() override;
+
+	/** Update */
+	virtual void Tick(float DeltaSeconds) override;
+
+	/** Returns the camera component **/
+	FORCEINLINE class UCameraComponent* GetTopDownCameraComponent() const { return TopDownCameraComponent; }
+
+	/** Returns the Camera Boom component **/
+	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
 	
 };
